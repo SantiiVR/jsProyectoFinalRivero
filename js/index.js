1bf9839bf322cd8ca0734509716701c18ef54761
Santiago Rivero <santiirv.1509@gmail.com>
2024-10-06 22:49:11 -0300

    se debugea el programa

--- js/index.js
+++ js/index.js
@@ -3,61 +3,90 @@
 let jugador;
 let pc;
 
+let puntosJugador =0
+
+let puntosPc =0
+let ronda=1
 
 
 function aleatoria() {
-    return Math.ceil (Math.random()*3)
+    return ""+Math.ceil (Math.random()*3)
 }
+
+
+
+
+function eleccion (jugador){
+switch (jugador) {
+    case "1":
+        return "Piedra"
+        break;
+    case "2":
+        return "Papel"
+        break;
+    
+    case "3":
+        return "Tijera"
+        break;
+
+        case "4":
+            return "Fin de juego"
+            break;
+
+    default:
+        return "selecciona una opcion valida"
+        break;
+}
+}
+
+function ganaJugador(){
+    puntosJugador++
+}
+function ganoPc() {
+    puntosPc++
+}
+
+do{
+console.log ("-------------- " + ronda+ " ----------------")
+
+    console.log ("tus puntos "+puntosJugador)
+
+    console.log ("los puntos del pc son " +puntosPc)
 
 jugador = prompt ("Elije: 1-Piedra, 2-Papel, 3-Tijera 4-cerrar");
 console.log("tu eleccion fue: "+ eleccion (jugador))
-pc = aleatoria (); 
+
+if (jugador==="1" || jugador==="2" || jugador==="3"){
+    ronda++
+    pc = aleatoria(); 
 console.log("La pc eligio: " + eleccion (pc))
 
-for (jugador=prompt; jugador===4; prompt){
-
-}
-
-function eleccion (jugada){
-    let resultado = " "
-    if (jugada == 1){
-        resultado = "Piedra"}
-
-    else if (jugada == 2){
-        resultado = "Papel"}
-
-        else if (jugada == 3){
-            resultado = "Tijera"}
-        
-    else (jugada = "Eleccion incorrecta")
-    return resultado
-}
 
 
 if (jugador == pc){
     console.log("Empate")
 }
 else if (jugador==1 && pc==3){
-    console.log("Ganaste");
     ganaJugador();
+    console.log("Ganaste");
+
 }
 else if (jugador==2 && pc==1){
-    console.log("Ganaste")
     ganaJugador();
+    console.log("Ganaste")
+
 } 
 else if (jugador==3 && pc==2){
-    console.log("Ganaste")
     ganaJugador();
+    console.log("Ganaste")
 }
+
 else{
-    console.log("Perdiste")
-    puntosPc();
+    ganoPc();
+    console.log("Perdiste");
+    
+};
 }
 
-
-function ganaJugador( ) {
-    puntosJugador++;
-}
-function ganapc(  ) {
-    puntosPc++;
+} while ( jugador!=4){
 }
